ec9399afbe370b98ac9e893bc4034cb9f11e2f80
Tomas Neme <lacrymology@gmail.com>
2018-04-06 18:24:53 -0300

    handle exceptions

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from exceptions import HttpError
 
 app = Flask('shiphero')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+@app.errorhandler(HttpError)
+def handle_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
