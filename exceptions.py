e6ec438fd63ec7bd960cd9f149af82b496d9bdae
Tomas Neme <lacrymology@gmail.com>
2018-04-06 18:24:41 -0300

    make API exceptions more useful

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,17 @@
 class HttpError(Exception):
     status_code=400
 
-    def __init__(self, message='Bad Request', code=None):
+    def __init__(self, message='Bad Request', code=None, payload=None):
+        super(Exception, self).__init__()
+        self.message = message
         if code is not None:
             self.status_code = code
-        self.message = message
+        self.payload = payload
+
+    def to_dict(self):
+        ret = dict(self.payload or ())
+        ret['message'] = self.message
+        return ret
 
 
 class NotFoundError(HttpError):
